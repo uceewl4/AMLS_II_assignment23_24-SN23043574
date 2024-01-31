f385c1a77bdccf610840adf1f47c83adf68b75c7
uceewl4 <uceewl4@ucl.ac.uk>
2024-01-31 11:20:18 +0000

    Finish all data preprocessing, construction of CNN and structure of the project.

--- image_classification/MoE.py
+++ image_classification/MoE.py
@@ -6,3 +6,7 @@ LastEditTime: 2024-01-30 19:41:19
 FilePath: /AMLS_II_assignment23_24-SN23043574/image_classification/MoE.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 """
+
+# split into furniture and home goods
+# look at the part misclassified
+# three experts: furnitu
